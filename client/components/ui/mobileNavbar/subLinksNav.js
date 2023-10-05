5b66ddcc52f8873334e758337f5e502237fb5f00
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-10-04 17:12:29 -0700

    fixed mobile menu

--- client/components/ui/mobileNavbar/subLinksNav.js
+++ client/components/ui/mobileNavbar/subLinksNav.js
@@ -3,16 +3,32 @@ import navbarStyles from "../navbar/navbar.module.css";
 import Link from "next/link";
 import RightArrow from "@/components/icons/rightArrow";
 import featuredCatStyles from "../../featuredCat/featuredCat.module.css";
+import { useState } from "react";
 
 const SubLinksNav = ({ sublinks, link, title }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+
   return (
     <>
-      <button className={navbarStyles.expand}>
+      <button className={navbarStyles.expand} onClick={handleOpen}>
         <ArrowLeft />
       </button>
-      <ul className={navbarStyles.subLinksMenu}>
+      <ul
+        className={navbarStyles.subLinksMenu}
+        style={
+          open ? { transform: "none" } : { transform: "translate(-100%, 0)" }
+        }
+      >
         <li>
-          <button className={navbarStyles.backButton}>
+          <button
+            className={navbarStyles.backButton}
+            onClick={() => setOpen(false)}
+          >
             <h3>Back</h3>
 
             <RightArrow />
