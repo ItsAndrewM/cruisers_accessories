a1c48ed56c3b74ccb61fd2f86e98f0a1b45c5f43
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-11-01 14:40:59 -0700

    removed weird asy

--- client/lib/hooks/useShippingRates.js
+++ client/lib/hooks/useShippingRates.js
@@ -1,24 +1,26 @@
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../context";
 
-export const useShippingRates = async () => {
-  // const [shippingRates, setShippingRates] = useState([]);
-  // const { swell } = useContext(Context);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const result = await swell.cart.getShippingRates();
-  //       setShippingRates(result.services);
-  //       if (!result) {
-  //         throw new Error(
-  //           "Product delivery requires 'shipment' value or country ISO code is required"
-  //         );
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-  // return shippingRates;
+export const useShippingRates = () => {
+  const [shippingRates, setShippingRates] = useState([]);
+  const { swell } = useContext(Context);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await swell.cart.getShippingRates();
+        setShippingRates(result.services);
+        if (!result) {
+          throw new Error(
+            "Product delivery requires 'shipment' value or country ISO code is required"
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return shippingRates;
 };
