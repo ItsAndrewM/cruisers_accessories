29b5a7d00db90488a581d14544cca41fba497852
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-11-29 21:22:00 -0800

    removed a bunch of test pagfe

--- client/pages/test/index.js
+++ client/pages/test/index.js
@@ -1,51 +1,14 @@
-import { useRouter } from "next/router";
-import Layout from "../../components/layout/layout";
-import styles from "../../styles/test.module.css";
-import swellConfig from "@/swell.config";
-import swell from "swell-js";
-import BreadCrumbs from "@/components/breadCrumbs/breadcrumbs";
-import { getProduct } from "@/lib/operations-swell";
-import Carousel from "@/components/carousel/carousel";
-import { useContext, useEffect, useState } from "react";
-import Link from "next/link";
-import navbarStyles from "@/components/ui/navbar/navbar.module.css";
-import CustomerInfo from "@/components/checkout/customerInfo/customerInfo";
-import Shipping from "@/components/checkout/shipping/shipping";
 import { getLayoutProps } from "@/lib/get-layout-props";
-import { useCart } from "@/lib/hooks/useCart";
-import { Context } from "@/lib/context";
 
 export async function getServerSideProps(context) {
-  const data = await fetch(
-    process.env.NODE_ENV === "production"
-      ? `${process.env.NEXT_PUBLIC_SITE_URL}/api/swell/shipping-method`
-      : "http://localhost:3000/api/swell/shipping-method"
-  );
-  const result = await data.json();
   return {
     props: {
-      product: "blah blah" || null,
       ...(await getLayoutProps()),
     },
   };
 }
 
-const Page = ({ product }) => {
-  const [aState, setAState] = useState();
-  const router = useRouter();
-  const { cart, swell } = useContext(Context);
-
-  useEffect(() => {
-    const fetchGetStuff = async () => {
-      const stuff = await swell.cart.getShippingRates();
-      console.log(stuff);
-      console.log(cart);
-    };
-    if (swell && cart) {
-      fetchGetStuff();
-    }
-  }, [swell, cart]);
-
+const Page = () => {
   return <div style={{ display: "flex", flexWrap: "nowrap" }}></div>;
 };
 
